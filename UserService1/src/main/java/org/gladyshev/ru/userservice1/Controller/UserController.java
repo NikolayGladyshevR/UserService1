93b9e2bead811f56b095895d4a7ad35427fd4bf3
Nikolay Gladyshev <Ddoser2000@yandex.ru>
2025-06-27 10:31:19 +0300

    Fix controller mistake

--- UserService1/src/main/java/org/gladyshev/ru/userservice1/Controller/UserController.java
+++ UserService1/src/main/java/org/gladyshev/ru/userservice1/Controller/UserController.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/api/users")
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
     @Autowired
     private UserService userService;
